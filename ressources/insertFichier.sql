c3b3957b4afd10b125ffcc7aea2e7d0737df1676
theo <theo.manfredi@sfr.fr>
2020-11-30 15:27:57 +0100

    ajout des insert Fichier et Utilisateurs

---
+++ ressources/insertFichier.sql
@@ -0,0 +1,35 @@
+insert into Fichier (taille, dateDepot, email) values (
+    25000,
+    to_date("07/03/2020", "dd/mm/yyyy"),
+    "theo.manfredi@grenoble-inp.fr"
+);
+
+insert into Fichier (taille, dateDepot, email) values (
+    760000,
+    to_date("13/04/2019", "dd/mm/yyyy"),
+    "theo.manfredi@grenoble-inp.fr"
+);
+
+insert into Fichier (taille, dateDepot, email) values (
+    3000000,
+    to_date("30/12/2010", "dd/mm/yyyy"),
+    "madelines.salles@grenoble-inp.fr"
+);
+
+insert into Fichier (taille, dateDepot, email) values (
+    4982901,
+    to_date("14/02/2018", "dd/mm/yyyy"),
+    "madelines.salles@grenoble-inp.fr"
+);
+
+insert into Fichier (taille, dateDepot, email) values (
+    378208,
+    to_date("28/11/2013", "dd/mm/yyyy"),
+    "andriy.parkhomenko@grenoble-inp.fr"
+);
+
+insert into Fichier (taille, dateDepot, email) values (
+    730275,
+    to_date("09/09/2019", "dd/mm/yyyy"),
+    "nicolas.vincent@grenoble-inp.fr"
+);
---
+++ ressources/insertUtilisateur.sql
@@ -0,0 +1,44 @@
+insert into Utilisateur values (
+    "theo.manfredi@grenoble-inp.fr",
+    "Manfredi",
+    "Theo",
+    22,
+    "Français",
+    "0123"
+);
+
+insert into Utilisateur values (
+    "nicolas.vincent@grenoble-inp.fr",
+    "Vincent",
+    "Nicolas",
+    22,
+    "Français",
+    "7777"
+);
+
+insert into Utilisateur values (
+    "madelines.salles@grenoble-inp.fr",
+    "Salles",
+    "Madelines",
+    21,
+    "Français",
+    "1999"
+);
+
+insert into Utilisateur values (
+    "adrien.argento@grenoble-inp.fr",
+    "Argento",
+    "Adrien",
+    22,
+    "Français",
+    "7402"
+);
+
+insert into Utilisateur values (
+    "andriy.parkhomenko@grenoble-inp.fr",
+    "Parkhomenko",
+    "Andriy",
+    22,
+    "Français",
+    "9387"
+);
