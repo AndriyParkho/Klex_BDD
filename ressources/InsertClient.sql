873f5ec8623704a7d81a1bdbb76eda7b371e2a3f
Adrien Argento <adrien.argento2@grenoble-inp.org>
2020-11-30 16:55:05 +0100

    Donnees pertinentes

---
+++ ressources/InsertClient.sql
@@ -0,0 +1,14 @@
+INSERT INTO Client
+VALUES ('Sony', 'M1', 720, 720);
+
+INSERT INTO Client
+VALUES ('Sony2', 'M2', 1080, 720);
+
+INSERT INTO Client
+VALUES ('Sony3', 'M3', 1920, 720);
+
+INSERT INTO Client
+VALUES ('Sony4', 'M4', 1920, 1080);
+
+INSERT INTO Client
+VALUES ('Sony5', 'M5', 1920, 1920);
---
+++ ressources/InsertCodec.sql
@@ -0,0 +1,32 @@
+INSERT INTO Codec
+VALUES ('MPEG2', 'video');
+
+INSERT INTO Codec
+VALUES ('MPEG4', 'video');
+
+INSERT INTO Codec
+VALUES ('DivX', 'video');
+
+INSERT INTO Codec
+VALUES ('H264', 'video');
+
+INSERT INTO Codec
+VALUES ('MP3', 'audio');
+
+INSERT INTO Codec
+VALUES ('MPEG4', 'audio');
+
+INSERT INTO Codec
+VALUES ('ACC', 'audio');
+
+INSERT INTO Codec
+VALUES ('ACC3', 'audio');
+
+INSERT INTO Codec
+VALUES ('AQTitle', 'texte');
+
+INSERT INTO Codec
+VALUES ('DKS', 'texte');
+
+INSERT INTO Codec
+VALUES ('Kate', 'texte');
