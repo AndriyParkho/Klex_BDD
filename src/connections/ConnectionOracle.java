b6bf70094c7c7a5ed99432bfae5b6475a8990232
NicovincX2 <nicolas.vincent100@gmail.com>
2020-12-01 10:17:18 +0100

    connection module

---
+++ src/connections/ConnectionOracle.java
@@ -0,0 +1,43 @@
+package src.connections;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import src.JDBCUtilities;
+
+public class ConnectionOracle {
+    private static final String url = "jdbc:oracle:thin:@oracle1.ensimag.fr:" + "1521:oracle1";
+    private static final String user = "vincentn";
+    private static final String passwd = "vincentn";
+    private static Connection connection = null;
+
+    // only gets called once, when the class itself is initialized, no matter how
+    // many objects of that type you create.
+    static {
+        // Enregistrement d’un pilote JDBC
+        System.out.println("Loading Oracle thin driver...");
+        try {
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+        } catch (SQLException e) {
+            System.err.println("sql error !");
+            JDBCUtilities.printSQLException(e);
+        }
+        System.out.println("loaded.");
+    }
+
+    public static Connection getInstance() {
+        if (connection == null) {
+            // Établissement d’une connexion et Requête Simple
+            System.out.println("Connecting to the database...");
+            try {
+                connection = DriverManager.getConnection(url, user, passwd);
+            } catch (SQLException e) {
+                System.err.println("sql error !");
+                JDBCUtilities.printSQLException(e);
+            }
+            System.out.println("connected.");
+        }
+        return connection;
+    }
+}
