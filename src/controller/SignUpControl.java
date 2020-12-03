e4e4e1a71c60e09bee991c0822c1f67cf3945d45
sallemad <madeline.salles@grenoble-inp.org>
2020-12-04 00:07:14 +0100

    controller sign up

--- src/controller/SignUpControl.java
+++ src/controller/SignUpControl.java
@@ -1,5 +1,11 @@
 package controller;
 
+import java.sql.SQLException;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
 import dao.DAOUtilisateur;
 import tables.Utilisateur;
 import views.SignUp;
@@ -20,6 +26,33 @@ public class SignUpControl {
 
 	public void clicValid() {
 		// TODO Auto-generated method stub
+		//email, nom, prenom,  age, langueDiffusion, code
+				String email = view.getMailField().getText();
+				try{
+					util = bddUtil.find(email);
+						
+				}catch(SQLException e) {
+					System.out.println("l'email existe deja");
+					JDialog erreur = new JDialog(view.getFenetre(),"erreur");
+					JLabel label = new JLabel("l'email existe deja", SwingConstants.CENTER);
+					erreur.add(label);
+					erreur.setSize(300, 100);
+					erreur.setLocationRelativeTo(null);
+					erreur.setVisible(true);
+				}
+				String nom = view.getNomField().getText();
+				String prenom = view.getPrenomField().getText();
+				//int age = Integer.parseInt(view.getAgeField().getText());
+				String langue = view.getLangueField().getText();
+				int code = Integer.parseInt(view.getCodeField().getText());
+				util = new Utilisateur(email, nom, prenom, 18 , langue, code, null);
+				try{
+					bddUtil.create(util);
+				}catch(SQLException e) {
+					System.out.println(e);
+				}
+				System.out.println(util);
+			
 		
 	}
 
